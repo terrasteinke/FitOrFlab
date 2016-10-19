2b902ee0d68ed5f8b53d99c83fa9f607f3abb962
terrasteinke <terrasteinke@gmail.com>
2016-10-19 04:12:37 -0400

    added logic to change activity_details based off of activity chosen

--- app/src/main/java/com/ubiquitousapps/fitorflab/detailsActivity.java
+++ app/src/main/java/com/ubiquitousapps/fitorflab/detailsActivity.java
@@ -1,8 +1,11 @@
 package com.ubiquitousapps.fitorflab;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class detailsActivity extends AppCompatActivity {
@@ -12,7 +15,22 @@ public class detailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        TextView exerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
+        TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);
         ImageView exerciseImage = (ImageView)findViewById(R.id.exerciseImage);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBg);
+
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        exerciseText.setText(exerciseTitle);
+
+        if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS)) {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.weight, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        } else if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)) {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.lotus, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+        } else (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_CARDIO)) {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.heart, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52AD56"));
+        }
     }
 }
