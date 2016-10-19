b5ac734133933ce7ec2b771492b94c1c0843b3e3
terrasteinke <terrasteinke@gmail.com>
2016-10-19 03:09:36 -0400

    fixed missing piece of loadDetailActivity function

--- app/src/main/java/com/ubiquitousapps/fitorflab/MainActivity.java
+++ app/src/main/java/com/ubiquitousapps/fitorflab/MainActivity.java
@@ -2,12 +2,46 @@ package com.ubiquitousapps.fitorflab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXERCISE_WEIGHTS = "weight lifting";
+    public static final String EXERCISE_YOGA = "yoga";
+    public static final String EXERCISE_CARDIO = "cardio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout weightBtn = (RelativeLayout) findViewById(R.id.weightBtn);
+        RelativeLayout yogaBtn = (RelativeLayout) findViewById(R.id.yogaBtn);
+        RelativeLayout cardioBtn = (RelativeLayout) findViewById(R.id.cardioBtn);
+
+        weightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(MainActivity.EXERCISE_WEIGHTS);
+            }
+        });
+
+        yogaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(MainActivity.EXERCISE_YOGA);
+            }
+        });
+
+        cardioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(MainActivity.EXERCISE_CARDIO);
+            }
+        });
+
+    private void loadDetailActivity(String exerciseTitle) {
+
     }
 }
