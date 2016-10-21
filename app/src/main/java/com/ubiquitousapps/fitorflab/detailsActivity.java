8b17ab475f8854ead8a0f5870e44d70db12302f0
terrasteinke <terrasteinke@gmail.com>
2016-10-21 01:43:41 -0400

    added settings page

--- app/src/main/java/com/ubiquitousapps/fitorflab/detailsActivity.java
+++ app/src/main/java/com/ubiquitousapps/fitorflab/detailsActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,6 +20,7 @@ public class detailsActivity extends AppCompatActivity {
         TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);
         ImageView exerciseImage = (ImageView)findViewById(R.id.exerciseImage);
         LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBg);
+        Button settingsBtn = (Button)findViewById(R.id.settingsBtn);
 
         String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
         exerciseText.setText(exerciseTitle);
@@ -32,5 +35,19 @@ public class detailsActivity extends AppCompatActivity {
             exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.heart, getApplicationContext().getTheme()));
             mainBG.setBackgroundColor(Color.parseColor("#52AD56"));
         }
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadSettingsActivity();
+            }
+        });
+
     }
-}
+
+    private void loadSettingsActivity() {
+        Intent intent = new Intent(detailsActivity.this, settingsActivity.class);
+        startActivity(intent);
+
+    }
+};
